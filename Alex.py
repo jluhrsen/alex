94cfac84e914cfef162f76babc7aa05e86d5f056
Jamo Luhrsen <jluhrsen@gmail.com>
2020-02-11 10:28:43 -0800

    Add python script to print Alex's name
    Signed-off-by: Jamo Luhrsen <jluhrsen@gmail.com>

---
+++ Alex.py
@@ -0,0 +1,17 @@
+"""
+    Alexander David Snoke
+
+      In Loving Memory
+        2005 - 2019
+"""
+
+from termcolor import cprint
+from pyfiglet import figlet_format
+
+__author__ = 'Jamo Luhrsen'
+
+in_memory_of = 'A L E X'
+
+color = 'red'
+
+cprint(figlet_format(in_memory_of, font="doh", width=116), color)
